77564f906f92e255d22bef08ce58282aca139f25
wcw <344078971@qq.com>
2025-03-11 00:10:23 +0800

    refactor(dashboard): update main navigation and settings routes

--- src/app/(dashboard)/dashboard/components/main-nav.tsx
+++ src/app/(dashboard)/dashboard/components/main-nav.tsx
@@ -1,37 +1,61 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/registry/new-york/ui/button";
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
       <Link
-        href="/examples/dashboard"
-        className="text-sm font-medium transition-colors hover:text-primary"
+        href="/dashboard"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          pathname === "/dashboard" ? "" : "text-muted-foreground",
+          "text-sm font-medium transition-colors hover:text-primary",
+        )}
       >
         Overview
       </Link>
+
       <Link
-        href="/examples/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/customers"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          pathname === "/customers" ? "" : "text-muted-foreground",
+          "text-sm font-medium transition-colors hover:text-primary",
+        )}
       >
         Customers
       </Link>
       <Link
-        href="/examples/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/products"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          pathname === "/products" ? "" : "text-muted-foreground",
+          "text-sm font-medium transition-colors hover:text-primary",
+        )}
       >
         Products
       </Link>
+
       <Link
-        href="/examples/dashboard"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/settings"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          pathname === "/settings" ? "" : "text-muted-foreground",
+          "text-sm font-medium transition-colors hover:text-primary",
+        )}
       >
         Settings
       </Link>
